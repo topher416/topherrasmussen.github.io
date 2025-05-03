f65bcf57f6abcb062c9683eac7122b5b869faef5
topher416 <topher416@gmail.com>
2025-05-03 17:29:17 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Add smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            
-            const targetId = this.getAttribute('href');
